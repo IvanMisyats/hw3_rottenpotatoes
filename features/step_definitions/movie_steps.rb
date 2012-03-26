c2099903dc21e3fed3ef74f3f3fe5be984e1c9b0
Ivan Misyats <ivan@misyats.com>
2012-03-26 13:35:22 -0700

    light changes

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,6 +1,6 @@
 # Add a declarative step here for populating the DB with movies.
 
-Given /the following movies exist/ do |movies_table|
+When /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
